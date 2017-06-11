8a6f9c7436cdc4e8a54a57dec785c5d85270e661
Andry Doroshenko <andry.dorosh@gmail.com>
2017-06-11 14:57:00 +0300

    add button's functional of listItem

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function createTodoItem(title) {
     checkbox.type = 'checkbox';
     checkbox.className = 'checkbox';
 
-    const  label = document.createElement('label');
+    const label = document.createElement('label');
     label.innerText = title;
     label.className = 'title';
 
@@ -27,13 +27,24 @@ function createTodoItem(title) {
     listItem.appendChild(editInput);
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
-
+    bindEvents(listItem);
     return listItem;
+}
+
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+
 }
 
 function addTodoItem(event) {
     event.preventDefault();
-    
+
     if (addInput.value === '') {
         return alert('Необходимо ввести название задачи.');
     }
@@ -41,6 +52,33 @@ function addTodoItem(event) {
     todoList.appendChild(todoItem);
     addInput.value = '';
 }
+
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        title.innerText = editInput.value;
+        this.innerText = 'Изменить';
+    } else {
+        editInput.value = title.innerText;
+        this.innerText = 'Сохранить';
+    }
+    listItem.classList.toggle('editing');
+}
+
+function deleteTodoItem() {
+    const listItem = this.parentNode;
+    todoList.removeChild(listItem);
+}
+
 
 const todoForm = document.getElementById('todo-form');
 const addInput = document.getElementById('add-input');
